92d836f533b8e9cc8da241d01e39f3a388004250
liem nguyen <elminguyen@gmail.com>
2024-03-05 01:07:52 +0200

    profile started

--- src/views/Profile.tsx
+++ src/views/Profile.tsx
@@ -1,13 +1,43 @@
-import {View} from 'react-native';
-import {Button} from '@rneui/base';
+import {
+  NavigationProp,
+  ParamListBase,
+  useNavigation,
+} from '@react-navigation/native';
+import {Button, Card, Icon, ListItem} from '@rneui/base';
 import {useUserContext} from '../hooks/contextHooks';
 
 const Profile = () => {
-  const {handleLogout} = useUserContext();
+  const {handleLogout, user} = useUserContext();
+  const navigation: NavigationProp<ParamListBase> = useNavigation();
   return (
-    <View>
-      <Button title="Logout" onPress={handleLogout} />
-    </View>
+    <>
+      {user && (
+        <Card>
+          <Card.Image source={{uri: 'https://placekitten.com/300/300'}} />
+          <ListItem>
+            <Icon name="person" />
+            <ListItem.Title>{user.username}</ListItem.Title>
+          </ListItem>
+          <ListItem>
+            <Icon name="email" />
+            <ListItem.Title>{user.email}</ListItem.Title>
+          </ListItem>
+          <ListItem>
+            <ListItem.Title>user id: {user.user_id}</ListItem.Title>
+          </ListItem>
+          <Card.Divider />
+          <Button onPress={() => navigation.navigate('My Files')}>
+            My Files &nbsp;
+            <Icon name="folder" color="white" />
+          </Button>
+          <Card.Divider />
+          <Button onPress={handleLogout}>
+            Logout &nbsp;
+            <Icon name="logout" color="white" />
+          </Button>
+        </Card>
+      )}
+    </>
   );
 };
 

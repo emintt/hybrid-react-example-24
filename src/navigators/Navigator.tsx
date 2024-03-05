c09d8e974c05b7e71fc46c18c1b03e212345c027
liem nguyen <elminguyen@gmail.com>
2024-03-05 09:28:33 +0200

    add image picker

--- src/navigators/Navigator.tsx
+++ src/navigators/Navigator.tsx
@@ -7,6 +7,7 @@ import Single from '../views/Single';
 import {useUserContext} from '../hooks/contextHooks';
 import Login from '../views/Login';
 import MyFiles from '../views/MyFiles';
+import Upload from '../views/Upload';
 
 const Tab = createBottomTabNavigator();
 const Stack = createNativeStackNavigator();
@@ -16,6 +17,7 @@ const TabNavigator = () => {
     <Tab.Navigator>
       <Tab.Screen name="Home" component={Home} />
       <Tab.Screen name="Profile" component={Profile} />
+      <Tab.Screen name="Upload" component={Upload} />
     </Tab.Navigator>
   );
 };
@@ -34,6 +36,7 @@ const StackNavigator = () => {
           />
           <Stack.Screen name="Single Media" component={Single} />
           <Stack.Screen name="My Files" component={MyFiles} />
+
         </>
       ) : (
         <Stack.Screen name="Login" component={Login} />
